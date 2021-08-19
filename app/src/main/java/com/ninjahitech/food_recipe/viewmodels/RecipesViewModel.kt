36e10ae84e39e2751cbb969c7620354440f8130c
phucquoc-ng <phucquoc.ng@gmail.com>
2021-08-19 07:11:00 +0700

    add data store repository

--- app/src/main/java/com/ninjahitech/food_recipe/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/ninjahitech/food_recipe/viewmodels/RecipesViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.ninjahitech.food_recipe.Constants
 import com.ninjahitech.food_recipe.Constants.Companion.API_KEY
+import com.ninjahitech.food_recipe.Constants.Companion.DEFAULT_DIET_TYPE
+import com.ninjahitech.food_recipe.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.ninjahitech.food_recipe.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -20,10 +23,10 @@ class RecipesViewModel @Inject constructor (
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
-        queries[Constants.QUERY_NUMBER] = "50"
+        queries[Constants.QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[Constants.QUERY_API_KEY] = API_KEY
-        queries[Constants.QUERY_TYPE] = "snack"
-        queries[Constants.QUERY_DIET] = "vegan"
+        queries[Constants.QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[Constants.QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[Constants.QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
         return queries
